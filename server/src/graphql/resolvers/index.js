514556aa724332d61f6b89230b80e9bfd3e662a5
Alex Ramos <xande.ramos93@gmail.com>
2018-04-12 00:20:32 -0700

    new user schema, and login function

--- server/src/graphql/resolvers/index.js
+++ server/src/graphql/resolvers/index.js
@@ -1,6 +1,8 @@
 import GraphQLDate from 'graphql-date';
 
 import TweetResolvers from './tweet-resolvers';
+import UserResolvers from './user-resolvers';
+import User from '../../models/User';
 
 export default {
     Date: GraphQLDate,
@@ -12,5 +14,7 @@ export default {
         createTweet: TweetResolvers.createTweet,
         updateTweet: TweetResolvers.updateTweet,
         deleteTweet: TweetResolvers.deleteTweet,
+        signup: UserResolvers.signup,
+        login: UserResolvers.login,
     }
 }
